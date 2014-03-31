06059d8607d2d31850e8ca0ef6ec7c6ecb2ef354
Daniel Pecos Martinez <contact@danielpecos.com>
2014-03-31 16:25:01 +0200

    Timings for Factorial (scala)

--- FactorialFPTCO.scala
+++ FactorialFPTCO.scala
@@ -1,6 +1,6 @@
 import scala.annotation._
 
-//@tailrec 
+@tailrec 
 def factorial_tco(n: Int, accumulator: BigInt = 1): BigInt = {
     if(n == 0) accumulator else factorial_tco(n - 1, (accumulator * n))
 }
@@ -10,5 +10,11 @@ def factorial(n: Int): BigInt = {
     
 }
 
+val start = System.nanoTime
 println(factorial_tco(10000))
+println((System.nanoTime - start) / 1000 / 1000)
+
+val start2 = System.nanoTime
 println(factorial(10000))
+println((System.nanoTime - start2) / 1000 / 1000)
+
